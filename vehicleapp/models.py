9824f19fefaf76c65b1c2a647883c7e9a3e01145
smithA10 <sterlinga01@gmail.com>
2018-05-01 18:06:08 -0400

    make changes

--- vehicleapp/models.py
+++ vehicleapp/models.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save
 class OwnerProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
 
-    addresss = models.CharField(max_length=150)
+    address = models.CharField(max_length=150)
 
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
